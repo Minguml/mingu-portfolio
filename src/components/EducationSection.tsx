40beb9754e09e7f616f20f7db92205380eb63a3e
Hoang Nhat Minh Vu <vhnm3004@outlook.com>
2025-06-02 11:18:19 +0700

    Changes to styles

--- src/components/EducationSection.tsx
+++ src/components/EducationSection.tsx
@@ -1,10 +1,10 @@
 export default function EducationSection() {
     return (
         <div className="flex flex-col m-auto gap-y-2">
-            <h1 className="text-white text-lg md:text-2xl">
+            <h1 className="text-white text-lg md:text-3xl">
                 Place of Education
             </h1>
-            <h2 className="text-almond text-2xl md:text-4xl">
+            <h2 className="text-almond text-lg md:text-3xl">
                 Ho Chi Minh City's University of Information Technology (UIT)
             </h2>
             <h1 className="text-white text-lg md:text-2xl">IELTS</h1>
